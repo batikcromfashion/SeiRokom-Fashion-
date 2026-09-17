e6707a4536341b6432c765c72a8ada236f59fd82
batikcromfashion <batikcromfashion@gmail.com>
2026-09-17 23:53:00 +0600

    Add Firebase configuration and Firestore setup

---
+++ firebase-config.js
@@ -0,0 +1,41 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { 
+  getFirestore, 
+  collection, 
+  addDoc, 
+  getDoc, 
+  getDocs, 
+  doc, 
+  updateDoc, 
+  query, 
+  where, 
+  orderBy, 
+  serverTimestamp 
+} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCdDiP0NAtmSQt5a8I-DEY8G44rAp4z_-M",
+  authDomain: "seirokom-fashion.firebaseapp.com",
+  projectId: "seirokom-fashion",
+  storageBucket: "seirokom-fashion.firebasestorage.app",
+  messagingSenderId: "1051300000338",
+  appId: "1:1051300000338:web:39bac8b1926eac61fc658b",
+  measurementId: "G-TB2Y5T78JD"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { 
+  db, 
+  collection, 
+  addDoc, 
+  getDoc, 
+  getDocs, 
+  doc, 
+  updateDoc, 
+  query, 
+  where, 
+  orderBy, 
+  serverTimestamp 
+};
